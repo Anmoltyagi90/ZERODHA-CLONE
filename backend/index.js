3c808d429c8c05901bc2a86e8c0845d19635203d
Anmol Tyagi <anmoltyagi675@gmail.com>
2025-11-12 15:06:00 +0530

    Add empty line for better code readability

--- backend/index.js
+++ backend/index.js
@@ -11,6 +11,7 @@ const { HoldingsModel } = require("./model/HoldingsModel");
 const { PositionsModel } = require("./model/PositionsModel");
 const { OrdersModel } = require("./model/OrdersModel");
 
+
 const PORT = process.env.PORT || 3002;
 const uri = process.env.MONGODB_URI;
 const JWT_SECRET = process.env.JWT_SECRET || "development-secret";
